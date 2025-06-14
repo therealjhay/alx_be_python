ea4fce5485ae569279eaea37a467c0a6c09f645c
therealjhay <92530531+therealjhay@users.noreply.github.com>
2025-06-14 22:52:31 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@
 # Factor to convert Fahrenheit to Celsius: (F - 32) * 5/9
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 # Factor to convert Celsius to Fahrenheit: (C * 9/5) + 32
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR\s*=\s*9\/5
 
 def convert_to_celsius(fahrenheit: float) -> float:
     """
